b358edbe9a97c79f1883fe83cba12e478d4de69a
Henrique Negri <henriquenegri84@gmail.com>
2021-04-22 11:43:29 -0300

    first commit

---
+++ src/services/UserService.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
+
+class UserService {
+  async create(email: string) {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userAlreadyExists = await usersRepository.findOne({ email })
+
+    if(userAlreadyExists) {
+      throw new Error("User already exists!")
+    }
+    
+    const user = usersRepository.create({
+      email
+    })
+  
+    await usersRepository.save(user); 
+
+    return user;
+  }
+}
+
+export { UserService }
